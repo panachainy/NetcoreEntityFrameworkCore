4149c1a47adf77b5036a0e4508bf2a0dc9543e41
Panachai <panachai.ny@gmail.com>
2018-06-14 21:19:51 +0700

    build

--- Models/DB/TrainWebAPIContext.cs
+++ Models/DB/TrainWebAPIContext.cs
@@ -24,7 +24,7 @@ namespace NetCore_webapi_efcore.Models.DB
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+// #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
                 optionsBuilder.UseSqlServer("Server=Localhost;Database=TrainWebAPI;Trusted_Connection=True;");
             }
         }
